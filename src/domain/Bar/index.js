011a258bf7b32fa55fba38fab6c2bcf6134f23f1
Lucas Lopes <lucasaferrlopes@gmail.com>
2019-03-18 22:30:18 -0300

    Implement TDD's step 2 to the Bar's constructor testing

--- src/domain/Bar/index.js
+++ src/domain/Bar/index.js
@@ -1,17 +1,68 @@
 module.exports = ({
   name, description, address,
 }) => {
+  const proto = {
+    getMenu() { return [...this.menu]; },
+    addMenuPhoto(menuPhoto) {
+      if (Buffer.isBuffer(menuPhoto)) {
+        this.menu.push(menuPhoto);
+      }
+    },
+    getPhotos() { return [...this.photos]; },
+    addPhoto(photo) {
+      if (Buffer.isBuffer(photo)) {
+        this.photos.push(photo);
+      }
+    },
+  };
   const properties = {
     name: {
-      value: name,
+      configurable: false,
+      get: () => this.name,
+      set: (value) => {
+        if (typeof value !== 'string' || value.length > 64) {
+          throw new Error('invalid data');
+        }
+        this.name = value;
+      },
     },
     description: {
-      value: description,
+      configurable: false,
+      get: () => this.description,
+      set: (value) => {
+        if (typeof value !== 'string' || value.length > 128) {
+          throw new Error('invalid data');
+        }
+        this.description = value;
+      },
     },
     address: {
-      value: address,
+      configurable: false,
+      get: () => this.address,
+      set: (value) => {
+        if (typeof value !== 'string' || value.length > 128) {
+          throw new Error('invalid data');
+        }
+        this.address = value;
+      },
+    },
+    menu: {
+      value: [],
+      configurable: false,
+      writtable: false,
+    },
+    photos: {
+      value: [],
+      configurable: false,
+      writtable: false,
     },
   };
 
-  return Object.create(null, properties);
+  const bar = Object.create(proto, properties);
+
+  bar.name = name;
+  bar.description = description;
+  bar.address = address;
+
+  return bar;
 };
